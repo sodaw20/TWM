ed4310c3293774ec95f6e165db16c09120834db3
ziolkoplays <201670758+ziolkoplayscode@users.noreply.github.com>
2026-01-03 14:14:34 -0500

    Update basic.py

--- TWM/cogs/basic.py
+++ TWM/cogs/basic.py
@@ -1106,14 +1106,14 @@ class Basic(Cog):
         # I dont care.
         # Fix it if you want, I made this at 2 AM
         # At least it doesn't require a website that shuts down when someone makes the bot's repository private..
-        if ext:
-            cog = self.bot.get_cog(ext)
+        cog = self.bot.get_cog(ext)
+        if cog:
             commands = cog.get_commands()
             if commands:
                 commandlist = [c.name for c in commands]
                 if ctx.guild:
                     await ctx.author.send("Run `pls help [command]` to see what a command does.")
-                    await ctx.author.send(f"List of commands in {ext}")
+                    await ctx.author.send(f"List of commands in {await ext}:")
                     await ctx.author.send("\n".join(commandlist))
                     return await ctx.reply(
                         content="As to not be rude, I have DMed the cog info to you.",
@@ -1121,7 +1121,7 @@ class Basic(Cog):
                     )
                 else:
                     await ctx.author.send("Run `pls help [command]` to see what a command does.")
-                    await ctx.author.send(f"List of commands in {cog}")
+                    await ctx.author.send(f"List of commands in {await ext}:")
                     await ctx.author.send("\n".join(commandlist))
             else:
                 await ctx.send("That cog has no commands!")
