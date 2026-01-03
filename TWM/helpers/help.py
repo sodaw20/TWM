845a77d192322c86fb3332f0538dcbb6b477f1c3
ziolkoplays <201670758+ziolkoplayscode@users.noreply.github.com>
2026-01-03 14:31:35 -0500

    Update help.py

--- TWM/helpers/help.py
+++ TWM/helpers/help.py
@@ -14,5 +14,10 @@ def get_commands(bot, cog):
     command_list = [c.name for c in cog.get_commands()]
     sorted_command_list = sorted(command_list, key=str.lower)
     command_message = "\n".join(sorted_command_list)
+    filtered_command_list = []
+
+    for n in sorted_command_list:
+        segments = bot.get_command(n).help.split("\n\n")
+        filtered_command_list.append(n + segments[0])
     return command_message
 
