22e881a89e896f5540b2d96e329c88244dc14486
ziolkoplays <201670758+ziolkoplayscode@users.noreply.github.com>
2026-01-14 18:37:51 -0500

    Update warriorcats.py

--- TWM/cogs/warriorcats.py
+++ TWM/cogs/warriorcats.py
@@ -57,7 +57,7 @@ class Warriors(Cog):
         prefix = random_msg("prefix") if profile["wcprefix"] == None or mention != None else profile["wcprefix"]
         suffix = random_msg("suffix") if profile["wcsuffix"] == None or mention != None else profile["wcsuffix"]
         clan = random_msg("random_clans") if profile["wcclan"] == None or mention != None else profile["wcclan"]
-        clanemoji = placeholders["clan_emojis"][clan].format(**shorthands)+ " " + clan if profile["wcclan"] == None else clan
+        clanemoji = placeholders["clan_emojis"][clan].format(**shorthands)+ " " + clan if profile["wcclan"] == None or mention != None else clan
         await ctx.send(f"Your warrior name is {prefix+suffix} from {clanemoji}.")
         random.seed(a=None, version=2)
         
